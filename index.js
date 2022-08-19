15298da043b4dae270d6e56851b5a98ca4b83235
Harkelion <71925235+Harkelion@users.noreply.github.com>
2022-08-20 00:10:00 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ module.exports = function tsl(mod) {
   const abnormality = require("./abData.json");
 
   let isEnabled = true,
+    debug = false,
     skills_list = {},
     skills_mean = {},
     aspd = player["aspd"],
@@ -22,23 +23,19 @@ module.exports = function tsl(mod) {
     playerName = null,
     startTimeNewSkill = 0,
     startTimePreviousSkill = 0,
-    debug = false,
     myID = null,
     myName = null,
     playerId = null,
     animDuration = null,
-    playerHooked1 = null,
-    playerHooked2 = null,
-    playerHooked3 = null,
+    playerHooked1 = true,
+    playerHooked2 = true,
+    playerHooked3 = true,
     jobNumber = null,
-    skills_listPath = `./data/skills_list-${jobsName[jobNumber]}-${
-      playerName != null ? playerName : myName
-    }.json`,
-    skills_meanPath = `./data/skills_mean-${jobsName[jobNumber]}-${
-      playerName != null ? playerName : myName
-    }.json`;
+    abId,
+    skills_listPath,
+    skills_meanPath;
 
-  const skillsName = skillsData[jobsName[jobNumber]];
+  let skillsName = skillsData[jobsName[jobNumber]];
 
   command.add("list", (arg1, arg2) => {
     if (arg1 == null) {
@@ -52,6 +49,31 @@ module.exports = function tsl(mod) {
       );
     }
     if (arg1 == "save") {
+      let today = new Date();
+      skills_listPath =
+        "./save/skills_list-" +
+        jobsName[jobNumber] +
+        "-" +
+        (playerName != null ? playerName : myName) +
+        "-" +
+        today.getHours() +
+        "-" +
+        today.getMinutes() +
+        "-" +
+        today.getSeconds() +
+        ".json";
+      skills_meanPath =
+        "./save/skills_mean-" +
+        jobsName[jobNumber] +
+        "-" +
+        (playerName != null ? playerName : myName) +
+        "-" +
+        today.getHours() +
+        "-" +
+        today.getMinutes() +
+        "-" +
+        today.getSeconds() +
+        ".json";
       reformateSkillList();
       saveJsonData(skills_listPath, skills_list);
       calculMean(skills_list);
@@ -69,6 +91,7 @@ module.exports = function tsl(mod) {
       );
     }
     if (arg1 == "target") {
+      reset();
       if (arg2 != null) {
         command.message("Skills List target is now " + arg2);
         playerName = arg2;
@@ -84,13 +107,16 @@ module.exports = function tsl(mod) {
   mod.hook("S_LOGIN", 14, (event) => {
     if (!isEnabled) return;
     jobNumber = (event.templateId - 10101) % 100;
+    aspdDivider = jobNumber >= 8 ? 100 : event.attackSpeed;
+    skillsName = skillsData[jobsName[jobNumber]];
     myID = event.gameId;
-    myName = event.gameId;
-    debug ? console.log("playerId :" + event.gameId) : "";
+    myName = event.name;
+    debug ? console.log("your Id :" + event.gameId) : "";
+    debug ? console.log("your job :" + jobsName[jobNumber]) : "";
   });
 
   mod.hook("S_PLAYER_STAT_UPDATE", 14, (event) => {
-    if (!isEnabled || playerName != null) return;
+    if (!isEnabled || playerName != null || !player.inCombat) return;
     calculTrueAS(event.attackSpeed, event.attackSpeedBonus, 0);
     debug ? console.log("aspd :" + aspd) : "";
     debug ? console.log("aspdDivider :" + aspdDivider) : "";
@@ -102,7 +128,9 @@ module.exports = function tsl(mod) {
       playerHooked1 = true;
       playerId = event.gameId;
       jobNumber = (event.templateId - 10101) % 100;
-      mod.send("C_REQUEST_USER_PAPERDOLL_INFO", 4, {
+      aspdDivider = jobNumber >= 8 ? 100 : event.attackSpeed;
+      skillsName = skillsData[jobsName[jobNumber]];
+      mod.send("C_REQUEST_USER_PAPERDOLL_INFO", 3, {
         zoom: false,
         name: playerName,
       });
@@ -113,43 +141,65 @@ module.exports = function tsl(mod) {
     }
   });
 
-  mod.hook("S_USER_PAPERDOLL_INFO", 15, (event) => {
+  mod.hook("S_USER_PAPERDOLL_INFO", 11, (event) => {
     if (!isEnabled) return;
     if (!playerHooked2) {
       playerHooked2 = true;
       playerId = event.gameId;
       jobNumber = (event.templateId - 10101) % 100;
+      aspdDivider = jobNumber >= 8 ? 100 : event.attackSpeed;
+      skillsName = skillsData[jobsName[jobNumber]];
       playerName = event.name;
       command.message(playerName + " found ! Class : " + jobsName[jobNumber]);
     }
     if (playerName != null) {
-      calculTrueAS(event.attackSpeed, event.attackSpeedBonus, additionalAS);
+      calculTrueAS(event.attackSpeed, event.attackSpeedBonus);
+      debug ? console.log("aspd :" + aspd) : "";
+      debug ? console.log("aspdDivider :" + aspdDivider) : "";
     }
-    debug ? console.log("aspd :" + aspd) : "";
-    debug ? console.log("aspdDivider :" + aspdDivider) : "";
-    return;
   });
 
-  mod.hook("S_ABNORMALITY_BEGIN.", 4, (event) => {
-    if (!isEnabled) return;
-    if ((event.target = playerId)) {
-      if (!playerHooked3) {
-        additionalAS = additionalAS - abnormality[event.id].as;
-        playerHooked3 = true;
-      } else {
-        additionalAS = additionalAS + abnormality[event.id].as;
+  mod.hook(
+    "S_ABNORMALITY_BEGIN",
+    4,
+    // { order: 999999, filter: { fake: null, silenced: null, modified: null } },
+    (event) => {
+      if (!isEnabled) return;
+      abId = JSON.stringify(event.id);
+      // debug ? console.log("Id : " + abId) : "";
+      // debug ? console.log("Source : " + event.source) : "";
+      // debug ? console.log("target : " + event.target) : "";
+      if (!Object.keys(abnormality).includes(abId)) return;
+      if (playerId == null) return;
+      if (event.source == playerId || event.target == playerId) {
+        debug ? console.log("Id : " + abId) : "";
+        debug ? console.log("Source : " + event.source) : "";
+        debug ? console.log("target : " + event.target) : "";
+        debug ? console.log("Name : " + abnormality[abId].name) : "";
+        additionalAS = abnormality[abId].as;
+        calculTrueAS(aspd * aspdDivider, additionalAS);
+        debug ? console.log("additionalAS : " + additionalAS) : "";
+        debug ? console.log("aspd :" + aspd) : "";
       }
-      debug ? console.log("additionalAS : " + additionalAS) : "";
     }
-  });
+  );
 
-  d.hook("S_ABNORMALITY_END.", 1, (event) => {
-    if (!isEnabled) return;
-    if ((event.target = playerId)) {
-      additionalAS = additionalAS - abnormality[event.id].as;
-      debug ? console.log("additionalAS : " + additionalAS) : "";
+  mod.hook(
+    "S_ABNORMALITY_END",
+    1,
+    { order: 999999, filter: { fake: null, silenced: null, modified: null } },
+    (event) => {
+      if (!isEnabled) return;
+      abId = JSON.stringify(event.id);
+      if (!Object.keys(abnormality).includes(abId)) return;
+      if (event.source == playerId || event.target == playerId) {
+        additionalAS = -abnormality[abId].as;
+        calculTrueAS(aspd * aspdDivider, additionalAS);
+        debug ? console.log("additionalAS : " + additionalAS) : "";
+        debug ? console.log("aspd :" + aspd) : "";
+      }
     }
-  });
+  );
 
   mod.hook(
     "S_ACTION_STAGE",
@@ -160,11 +210,11 @@ module.exports = function tsl(mod) {
       if (myID == event.gameId && player.inCombat) {
         debug ? console.log("My Skill :" + event.gameId) : "";
         startTimeNewSkill = Date.now();
-        animDuration = event.animSeq[0];
+        // animDuration = event.animSeq[0];
         debug
           ? console.log("Skill id :" + Math.floor(event.skill.id / 10000))
           : "";
-        addSkill(Math.floor(event.skill.id / 10000));
+        addSkill(event.skill.id);
         // previousSkill = event.skill.id;
       }
     }
@@ -175,12 +225,11 @@ module.exports = function tsl(mod) {
     9,
     { order: 999999, filter: { fake: null, silenced: null, modified: null } },
     (event) => {
-      debug ? console.log("player used a Skill :" + event.gameId) : "";
       if (!isEnabled) return;
       if (playerId == event.gameId) {
         debug ? console.log("Target use Skill :" + event.gameId) : "";
         startTimeNewSkill = Date.now();
-        animDuration = event.animSeq[0];
+        // animDuration = event.animSeq[0];
         debug
           ? console.log("Skill id :" + Math.floor(event.skill.id / 10000))
           : "";
@@ -193,19 +242,19 @@ module.exports = function tsl(mod) {
 
   // });
 
-  function calculTrueAS(defaultAS, bonusAS, additionalAS) {
-    aspdDivider = jobNumber >= 8 ? 100 : defaultAS;
-    aspd = (defaultAS + bonusAS + additionalAS) / aspdDivider;
+  function calculTrueAS(defaultAS, bonusAS) {
+    aspd = Math.round(defaultAS + bonusAS) / aspdDivider;
   }
 
   function addSkill(skillId) {
     skills_list[skillNumber] = {
-      skillName: `${skillsName[Math.floor(skillId / 10000)]}-${skillId % 100}`,
-      SkillId: skillId,
+      skillName:
+        skillsName[Math.floor(skillId / 10000)] + "-" + (skillId % 100),
+      Skill_Id: skillId,
       castTime:
         skillNumber == 0 ? 0 : startTimeNewSkill - startTimePreviousSkill,
       currentSpeed: aspd * 100,
-      animDuration: animDuration,
+      // animDuration: animDuration,
       animCalculed: null,
     };
     debug ? console.log("castTime :" + skills_list[skillNumber].castTime) : "";
@@ -216,57 +265,55 @@ module.exports = function tsl(mod) {
     skillNumber++;
   }
 
-  for (const id in skillsName) {
-    skills_mean[skillsName[id]] = {
-      castTimeMini: 99999,
-      allAnimCalculedTime: 0,
-      NumberOfCast: 0,
-      castTimeMean: 0,
-    };
-  }
-
   function calculMean(skills_list) {
+    for (const id in skillsName) {
+      skills_mean[skillsName[id]] = {
+        castTimeMini: 99999,
+        allAnimCalculedTime: 0,
+        NumberOfCast: 0,
+        castTimeMean: 0,
+      };
+    }
     for (const skillUsed in skills_list) {
       for (const id in skillsName) {
-        if (skills_list[skillUsed].skillName.includes(skillsName[id])) {
-          skills_mean[skills_list[skillUsed].skillName] = {
-            castTimeMini:
-              skills_mean[skillsName[id]].castTimeMini <
-              skills_list[skillUsed].castTime
-                ? skills_mean[skillsName[id]].castTimeMini
-                : skills_list[skillUsed].castTime,
-            allAnimCalculedTime:
-              skills_mean[skillsName[id]].allAnimCalculedTime +
-              skills_list[skillUsed].animCalculed,
-            NumberOfCast: skills_mean[skillsName[id]].NumberOfCast + 1,
-          };
+        if (skills_list[skillUsed].skillName) {
+          if (skills_list[skillUsed].skillName.includes(skillsName[id])) {
+            skills_mean[skills_list[skillUsed].skillName] = {
+              castTimeMini:
+                skills_mean[skillsName[id]].castTimeMini <
+                skills_list[skillUsed].castTime
+                  ? skills_mean[skillsName[id]].castTimeMini
+                  : skills_list[skillUsed].castTime,
+              allAnimCalculedTime:
+                skills_mean[skillsName[id]].allAnimCalculedTime +
+                skills_list[skillUsed].animCalculed,
+              NumberOfCast: skills_mean[skillsName[id]].NumberOfCast + 1,
+            };
+          }
         }
       }
     }
     for (const name in skills_mean) {
       if (skills_mean[name].NumberOfCast != 0) {
         skills_mean[name] = {
-          skillName: skills_mean[name].skillName,
-          // 2.08 is max att speed class
-          castTimeMini: Math.floor(skills_mean[name].castTimeMini * 2.08),
-          animationMean: Math.floor(
+          // * 2.08 is max att speed
+          castTimeMini: Math.floor(skills_mean[name].castTimeMini),
+          AnimationMean: Math.floor(
             skills_mean[name].allAnimCalculedTime /
-              skills_mean[name].NumberOfCast /
-              100
+              skills_mean[name].NumberOfCast
           ),
         };
       } else {
         delete skills_mean[name];
       }
     }
-    // skills_list = skills_list + skills_mean
   }
 
   function reformateSkillList() {
     for (const element in skills_list) {
-      if (skills_list[JSON.stringify(parseInt(element) + 1)]) {
+      if (skills_list[parseInt(element) + 1]) {
         skills_list[element].castTime =
-          skills_list[JSON.stringify(parseInt(element) + 1)].castTime;
+          skills_list[parseInt(element) + 1].castTime;
       } else {
         skills_list[element].castTime = 0;
       }
@@ -279,7 +326,7 @@ module.exports = function tsl(mod) {
     skills_list["Player info"] = {
       Name: playerName != null ? playerName : myName,
       job: jobsName[jobNumber],
-      Id: playerId != null ? playerId : myID,
+      Id: playerId != null ? parseInt(playerId) : parseInt(myID),
     };
   }
 
@@ -294,20 +341,15 @@ module.exports = function tsl(mod) {
     skills_list = {};
     skills_mean = {};
     aspd = player["aspd"];
-    aspdDivider = 100;
     additionalAS = 0;
     skillNumber = 0;
     playerName = null;
     startTimeNewSkill = 0;
     startTimePreviousSkill = 0;
-    debug = false;
-    myID = null;
-    myName = null;
     playerId = null;
     animDuration = null;
-    playerHooked1 = null;
-    playerHooked2 = null;
-    playerHooked3 = null;
-    jobNumber = null;
+    playerHooked1 = true;
+    playerHooked2 = true;
+    playerHooked3 = true;
   }
 };
